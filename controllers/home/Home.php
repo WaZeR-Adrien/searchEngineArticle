b86a43c2c3324d29dc855b8e4c15cd9dc62278f3
adrien <officielghostfire@gmail.com>
2017-06-02 16:26:03 +0200

    Update CSS

--- controllers/home/Home.php
+++ controllers/home/Home.php
@@ -6,18 +6,7 @@ class Home
 {
     public static function index($twig)
     {
-        /*$url = 'http://searchengine-article.dev/api/keyword';
-        if(function_exists('curl_version')){
-            $curl = curl_init($url);
-            $response = curl_exec($curl);
-        } else {
-            $response = file_get_contents($url);
-        }*/
-
-        Article::getByKeyword('play');
-        $array = [
-            /*'keyword' => json_decode($response)*/
-        ];
+        $array = [];
 
         return $twig->render('home.html.twig', $array);
     }
